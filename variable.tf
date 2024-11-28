a21ab023f5454c02e65f207424bdc708e188af61
Your Name <you@example.com>
2024-11-28 11:04:53 +0530

    msg

--- variable.tf
+++ variable.tf
@@ -1,3 +1,4 @@
+# mandatory ,user shouid supply
 variable "ami_id" {
     type = string
    # default = "ami-09c813fb71547fc4f" #variables will be override
@@ -5,7 +6,15 @@ variable "ami_id" {
 # optional default t3.micro .user can always override
 variable "instance_type" {
     default = "t3.micro"
+    type = string
+    validation {
+        condition = contains (["t3.micro","t3.medium","t3.small", var.test_variable])
+        error_message = "instance type can only be ."
+    }
+
 }
+
+
 # mandatory user shouid supply
 variable "security_group_ids"{
     type = list(string)
